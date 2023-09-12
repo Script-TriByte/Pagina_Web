bef7f2fe3c5fa375ba2b4066d18a8f2757700523
Maykol Martinez <mjoelmartinez320@gmail.com>
2023-09-12 16:16:07 -0300

    Cambio total de diseño

--- js/aplicacionAlmacenes.js
+++ js/aplicacionAlmacenes.js
@@ -1,24 +1,35 @@
 function revelarPreviewArticulos(){
-    document.body.style.backgroundImage = "url(/img/BGArticulos.png)";
+    $(".contenedorAccesoAListas").css("background-image", "url(/img/BGArticulos.png)");
     $("#iconoListadoArticulos").attr("src", "/img/iconoArticuloHover.png");
+    $("#botonListaPaquetes").css("opacity", "50%");
+    $("#botonListaLotes").css("opacity", "50%");
 }
 
 function revelarPreviewPaquetes(){
-    document.body.style.backgroundImage = "url(/img/BGPaquetes.png)";
+    $(".contenedorAccesoAListas").css("background-image", "url(/img/BGPaquetes.png)");
     $("#iconoListadoPaquetes").attr("src", "/img/iconoPaqueteHover.png");
+    $("#botonListaArticulos").css("opacity", "50%");
+    $("#botonListaLotes").css("opacity", "50%");
 }
 
 function revelarPreviewLotes(){
-    document.body.style.backgroundImage = "url(/img/BGLotes.png)";
+    $(".contenedorAccesoAListas").css("background-image", "url(/img/BGLotes.png)");
     $("#iconoListadoLotes").attr("src", "/img/iconoLoteHover.png");
+    $("#botonListaArticulos").css("opacity", "50%");
+    $("#botonListaPaquetes").css("opacity", "50%");
 }
 
 function restaurarBody(){
-    document.body.style.backgroundImage = "url(/img/BGAlmacen.png)";
+    $(".contenedorAccesoAListas").css("background-image", "url(/img/BGAlmacen.png)");
 
     $("#iconoListadoArticulos").attr("src", "/img/iconoArticulo.png");
+    $("#botonListaArticulos").css("opacity", "100%");
+    
     $("#iconoListadoPaquetes").attr("src", "/img/iconoPaquete.png");
+    $("#botonListaPaquetes").css("opacity", "100%");
+
     $("#iconoListadoLotes").attr("src", "/img/iconoLote.png");
+    $("#botonListaLotes").css("opacity", "100%");
 }
 
 $("#botonListaArticulos").mouseover(function() {
--- js/main.js
+++ js/main.js
@@ -12,8 +12,8 @@ if (aplicacionActual === "Almacen"){
 function revelarAplicacionAlmacen(){
   $("#nombreAplicacion").html("APLICACIÓN DE GESTIÓN <br> DE ALMACENES");
   $("#informacionApp").html("Gestione los artículos de su almacén, así como los paquetes y lotes con los que trabajar.");
-  document.body.style.backgroundImage = "url(/img/BGAlmacen.png)";
   $("#accesoAplicacion").attr("href", "/html/aplicaciones/appAlmacenes.html");
+  $(".contenedorInformacion").css("background-image", "url(/img/BGAlmacen.png)");
 
   ocultarFlechaIzquierda();
   mostrarFlechaDerecha();
@@ -23,8 +23,8 @@ function revelarAplicacionAlmacen(){
 function revelarAplicacionCamiones(){
   $("#nombreAplicacion").html("APLICACIÓN DE GESTIÓN <br> DE CAMIONES");
   $("#informacionApp").html("Observe el contenido de su vehículo, así como sus entregas pendientes y destinos a cargar.");
-  document.body.style.backgroundImage = "url(/img/BGCamiones.png)";
   $("#accesoAplicacion").attr("href", "/html/aplicaciones/appCamiones.html");
+  $(".contenedorInformacion").css("background-image", "url(/img/BGCamiones.png)");
 
   mostrarFlechaIzquierda();
   mostrarFlechaDerecha();
@@ -35,8 +35,8 @@ function revelarAplicacionCamiones(){
 function revelarAplicacionSeguimiento(){
   $("#nombreAplicacion").html("APLICACIÓN DE <br> SEGUIMIENTO");
   $("#informacionApp").html("Observe la lista de paquetes con destinos establecidos, con su respectiva información asignada.");
-  document.body.style.backgroundImage = "url(/img/BGSeguimiento.png)";
   $("#accesoAplicacion").attr("href", "/html/aplicaciones/appSeguimiento.html");
+  $(".contenedorInformacion").css("background-image", "url(/img/BGSeguimiento.png)");
 
   mostrarFlechaIzquierda();
   mostrarFlechaDerecha();
@@ -47,8 +47,8 @@ function revelarAplicacionSeguimiento(){
 function revelarBackoffice(){
   $("#nombreAplicacion").html("BACKOFFICE DE <br> ADMINISTRACIÓN");
   $("#informacionApp").html("Gestione los Usuarios del Sistema, así como todos los Almacenes y Vehículos en disposición.");
-  document.body.style.backgroundImage = "url(/img/BGBackoffice.png)";
   $("#accesoAplicacion").attr("href", urlBackoffice);
+  $(".contenedorInformacion").css("background-image", "url(/img/BGBackoffice.png)");
 
   mostrarFlechaIzquierda();
   ocultarFlechaDerecha();
